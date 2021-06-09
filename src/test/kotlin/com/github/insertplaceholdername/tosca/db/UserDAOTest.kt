9cdbdd2a33d1035d42607cba208bf728bc609a83
Fredrik Peteri <fredrik.peteri@gmail.com>
2021-06-09 20:57:36 +0200

    Adds Postgres container for Unit Test

--- src/test/kotlin/com/github/insertplaceholdername/tosca/db/UserDAOTest.kt
+++ src/test/kotlin/com/github/insertplaceholdername/tosca/db/UserDAOTest.kt
@@ -1,28 +1,52 @@
 package com.github.insertplaceholdername.tosca.db
 
 
-import org.junit.jupiter.api.BeforeEach
+import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.Before
 import org.junit.Test
+import org.junit.BeforeClass
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import kotlin.test.assertEquals
 
 class MyPostgreSQLContainer(imageName: String) : PostgreSQLContainer<MyPostgreSQLContainer>(imageName)
 
+
 @Testcontainers
 internal class UserDAOTest {
-    @Container
-    val postgresContainer = MyPostgreSQLContainer("postgres:13.3")
-        .withDatabaseName("db")
-        .withUsername("user")
-        .withPassword("password")
+    companion object {
+        @Container
+        val postgresContainer = MyPostgreSQLContainer("postgres:13.3")
+            .withDatabaseName("db")
+            .withUsername("postgres")
+            .withPassword("test")
 
-    @BeforeEach
+        @BeforeClass
+        @JvmStatic
+        fun migrateDatabase() {
+            postgresContainer.start()
+            val jdbcUrl = "jdbc:postgresql://${postgresContainer.host}:${postgresContainer.firstMappedPort}/${postgresContainer.databaseName}"
+
+            Flyway.configure()
+                .dataSource(jdbcUrl, postgresContainer.username, postgresContainer.password)
+                .load()
+                .migrate()
+
+            Database.connect(jdbcUrl, user = postgresContainer.username, password = postgresContainer.password)
+        }
+    }
+
+    @Before
     fun setUp() {
+        // TODO Clear tables
     }
 
     @Test
-    fun containerTest() {
-        postgresContainer.start()
+    fun getEmptyUserList() {
+        val users = transaction { UserDAO.all().toList() }
+        assertEquals(0, users.size)
     }
 }
