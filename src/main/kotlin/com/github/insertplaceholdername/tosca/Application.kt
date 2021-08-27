6859ca5b96c0adcebd3e16e59b1ff9bfe03327ad
Klas af Geijerstam <klas.afgeijerstam@slagkryssaren.com>
2021-08-27 19:13:09 +0200

    Switched to HOCON configuration of database

--- src/main/kotlin/com/github/insertplaceholdername/tosca/Application.kt
+++ src/main/kotlin/com/github/insertplaceholdername/tosca/Application.kt
@@ -3,11 +3,14 @@ package com.github.insertplaceholdername.tosca
 import com.github.insertplaceholdername.tosca.auth.Groups
 import com.github.insertplaceholdername.tosca.auth.JwtAuthorization
 import com.github.insertplaceholdername.tosca.auth.setupAuth
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.config.*
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.routing.get
@@ -15,18 +18,19 @@ import io.ktor.routing.routing
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
 
-fun configureDB() {
-    val config = if (!System.getenv("JDBC_DATABASE_URL").isNullOrEmpty()) {
+fun configureDB(applicationConfig: Config) {
+
+    val config = if (applicationConfig.tryGetString("database.jdbcUrl")?.isNotEmpty() == true) {
         HikariConfig().apply {
-            jdbcUrl = System.getenv("JDBC_DATABASE_URL")
+            jdbcUrl = applicationConfig.getString("database.jdbcUrl")
             maximumPoolSize = 10
         }
     } else {
         HikariConfig().apply {
-            jdbcUrl = "jdbc:postgresql://${System.getenv("POSTGRES_HOST")}:5432/postgres"
+            jdbcUrl = "jdbc:postgresql://${applicationConfig.getString("database.host")}:5432/postgres"
             driverClassName = "org.postgresql.Driver"
-            username = System.getenv("POSTGRES_USERNAME")
-            password = System.getenv("POSTGRES_PASSWORD")
+            username = applicationConfig.getString("database.user")
+            password = applicationConfig.getString("database.password")
             maximumPoolSize = 10
         }
     }
@@ -40,7 +44,7 @@ fun configureDB() {
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
-    configureDB()
+    configureDB(ConfigFactory.load() ?: throw Exception("Could not load config"))
     setupAuth(ExposedUserRepository)
 
     routing {
